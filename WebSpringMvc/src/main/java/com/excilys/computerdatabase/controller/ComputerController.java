bf4f68621caa816274c718262b4c5c73ba04fa8e
GabrielPlassard <gabriel.plassard@gmail.com>
2013-06-05 17:31:32 +0200

    Controller ok pour toutes les opérations

--- WebSpringMvc/src/main/java/com/excilys/computerdatabase/controller/ComputerController.java
+++ WebSpringMvc/src/main/java/com/excilys/computerdatabase/controller/ComputerController.java
@@ -1,14 +1,19 @@
 package com.excilys.computerdatabase.controller;
 
+import com.excilys.computerdatabase.form.ComputerForm;
 import com.excilys.computerdatabase.model.Computer;
+import com.excilys.computerdatabase.queryresults.ComputerAndCompanies;
 import com.excilys.computerdatabase.queryresults.ComputersAndTotalNumber;
 import com.excilys.computerdatabase.service.ComputerDatabaseService;
 import com.excilys.computerdatabase.utils.C;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,16 +33,15 @@ public class ComputerController {
 
 
     @RequestMapping("/computers")
-    public String printWelcome(ModelMap model,
-                               @RequestParam(value="s",defaultValue = "2") Integer sortedColumnNumber,
+    public String index(ModelMap model,
+                               @ModelAttribute("alertMessage") String alertMessage,
+                               @RequestParam(value="s",defaultValue = "2") int sortedColumnNumber,
                                @RequestParam(value="f",defaultValue = "") String research,
-                               @RequestParam(value="p",defaultValue = "1") Integer currentPage) {
+                               @RequestParam(value="p",defaultValue = "1") int currentPage) {
 
         if (Math.abs(sortedColumnNumber) < 2 || Math.abs(sortedColumnNumber) > 5) {
             sortedColumnNumber = 2;
         }
-
-        String alertMessage = "message d'alerte";
 
         int firstComputerIndice = (currentPage - 1) * C.COMPUTERS_PER_PAGE;
         int lastComputerIndice = firstComputerIndice + C.COMPUTERS_PER_PAGE;
@@ -46,7 +50,6 @@ public class ComputerController {
         int numberOfMatchingComputers = queryResult.getNumberOfMatchingComputers();
         List<Computer> computers = queryResult.getMatchingComputers();
         int maxPage = (int) Math.ceil((1.0 * numberOfMatchingComputers) / C.COMPUTERS_PER_PAGE);
-
 
         model.addAttribute("alertMessage", alertMessage);
         model.addAttribute("sorting", sortedColumnNumber);
@@ -58,5 +61,85 @@ public class ComputerController {
         model.addAttribute("totalComputersFound", numberOfMatchingComputers);
         model.addAttribute("computers", computers);
         return "computers";
+    }
+
+    @RequestMapping(value = "/computers/edit", method = RequestMethod.GET)
+    public String editComputer(ModelMap model, @RequestParam(value="id", defaultValue = "0") long id){
+        ComputerAndCompanies queryResult = computerDatabaseService.computerByIdAndCompanies(id);
+        ComputerForm form = new ComputerForm(queryResult.getComputer());
+
+        model.addAttribute("mode", "edit");
+        model.addAttribute("fieldValues", form.getFieldValues());
+        model.addAttribute("companies", queryResult.getCompanies());
+        return "computer";
+    }
+
+    @RequestMapping(value = "/computers/edit", method = RequestMethod.POST)
+    public String saveEdit(ModelMap model, final RedirectAttributes redirectAttributes,
+                                            @RequestParam(value="id", defaultValue = "0") long id,
+                                            @ModelAttribute(value="name") String name,
+                                            @ModelAttribute(value="introduced") String introduced,
+                                            @ModelAttribute(value="discontinued") String discontinued,
+                                            @ModelAttribute(value="company") String companyId){
+
+        ComputerForm form = new ComputerForm(name,introduced,discontinued,companyId);
+
+        if (form.isValid()) {
+            Computer computer = form.getComputer();
+            computer.setId(id);
+            boolean succesfull = computerDatabaseService.updateComputerAndSetCompany(computer, form.getCompanyId());
+            if (succesfull) {
+                redirectAttributes.addFlashAttribute("alertMessage", "Computer " + computer.getName() + " modified successfully");
+            } else {
+                redirectAttributes.addFlashAttribute("alertMessage", "There has been a problem while updating " + computer.getName());
+            }
+            return "redirect:../computers";
+        } else {
+            model.addAttribute("mode", "edit");
+            model.addAttribute("companies", computerDatabaseService.allCompanies());
+            model.addAttribute("errorMessages", form.getErrorMessages());
+            model.addAttribute("fieldValues", form.getFieldValues());
+            return "computer";
+        }
+    }
+
+    @RequestMapping(value ="/computers/new", method = RequestMethod.GET)
+    public String newComputer(ModelMap model){
+        model.addAttribute("mode", "new");
+        model.addAttribute("companies", computerDatabaseService.allCompanies());
+        return "computer";
+    }
+
+    @RequestMapping(value ="/computers/new", method = RequestMethod.POST)
+    public String saveNewComputer(ModelMap model,final RedirectAttributes redirectAttributes,
+                                  @ModelAttribute(value="name") String name,
+                                  @ModelAttribute(value="introduced") String introduced,
+                                  @ModelAttribute(value="discontinued") String discontinued,
+                                  @ModelAttribute(value="company") String companyId){
+
+        ComputerForm form = new ComputerForm(name,introduced,discontinued,companyId);
+
+        if (form.isValid()) {
+            redirectAttributes.addFlashAttribute("alertMessage", "Computer " + form.getComputer().getName() + " added successfully");
+            computerDatabaseService.createComputerAndSetCompany(form.getComputer(), form.getCompanyId());
+            return "redirect:../computers";
+        } else {
+            model.addAttribute("mode", "new");
+            model.addAttribute("companies", computerDatabaseService.allCompanies());
+            model.addAttribute("errorMessages", form.getErrorMessages());
+            model.addAttribute("fieldValues", form.getFieldValues());
+            return "computer";
+        }
+    }
+
+    @RequestMapping(value ="/computers/delete")
+    public String deleteComputer(final RedirectAttributes redirectAttributes,@RequestParam(value="id", defaultValue = "0") long computerId){
+        boolean succesfull = computerDatabaseService.deleteComputerById(computerId);
+        if (succesfull) {
+            redirectAttributes.addFlashAttribute("alertMessage", "Computer deleted successfully");
+        } else {
+            redirectAttributes.addFlashAttribute("alertMessage", "There has been a problem while deleting the computer");
+        }
+        return "redirect:../computers";
     }
 }
